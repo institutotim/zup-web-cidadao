8266b427d512256ad0ecd384ed62586663f908ba
Gabriel Araujo <gabrielqa@me.com>
2014-01-27 07:22:09 -0200

    #ZUP-163 #done Login de usuários

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,15 +4,118 @@ angular.module('zupWebAngularApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $httpProvider.interceptors.push(['$q', '$injector', function($q, $injector) {
+    return {
+      // change URL on external requests
+      'request': function(config) {
+        // temparary fix -- replace with http://staging.zup.sapience.io later
+        config.url = config.url.replace('{base_url}', 'http://staging.zup.sapience.io');
+
+        // get token and pass to the server with header X-App-Token
+        var token = null;
+
+        $injector.invoke(['Auth', function(Auth) {
+          token = Auth.getToken();
+        }]);
+
+        config.headers['X-App-Token'] = token;
+
+        // apply all the changes! :)
+        return config || $q.when(config);
+      },
+
+      // check for expected errors
+      // if not, show generic system error
+      'responseError': function(rejection) {
+
+        var expectedErrors = rejection.config.expectedErrors;
+
+        if (!(typeof expectedErrors !== 'undefined' && expectedErrors.indexOf(rejection.status) !== -1))
+        {
+          $injector.invoke(['Error', function(Error) {
+            Error.showDetails(rejection);
+          }]);
+        }
+
+        return $q.reject(rejection);
+      }
+    };
+  }]);
+
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+
+.run(['$rootScope', '$location', 'Auth', '$modal', function($rootScope, $location, Auth, $modal) {
+
+  $rootScope.$on('$routeChangeStart', function(e, curr, prev) {
+    if (typeof prev === 'undefined')
+    {
+      $rootScope.isLoading = true;
+    }
+
+    // Check if user has a cookie with token
+    var check = Auth.check();
+
+    check.then(function() {
+      // onSuccess
+      $rootScope.isLoading = false;
+      $rootScope.logged = true;
+    }, function() {
+      $rootScope.isLoading = false;
+      $rootScope.false = true;
+    });
   });
+
+  $rootScope.login = function() {
+    $modal.open({
+      templateUrl: 'views/modal_login.html',
+      windowClass: 'modal_login',
+      controller: ['$scope', '$modalInstance', 'User', function($scope, $modalInstance, User) {
+
+        $scope.input = {};
+
+        $scope.close = function () {
+          $modalInstance.close();
+        };
+
+        $scope.login = function() {
+          $scope.loginError = false;
+
+          var user = new User($scope.input.email, $scope.input.password);
+
+          user.auth().then(function() {
+            $rootScope.logged = true;
+            $modalInstance.close();
+          }, function(response) {
+            if (response.status === 400 || response.status === 401)
+            {
+              $scope.loginError = true;
+            }
+          });
+        };
+      }]
+    });
+  };
+
+  $rootScope.logout = function() {
+    Auth.clearToken();
+    Auth.saveUser(null);
+    $rootScope.logged = false;
+  };
+
+}]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
 angular.module('zupWebAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+.controller('MainCtrl', function () {
+
+});
