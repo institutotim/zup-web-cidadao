6559b569a56ccdbe5330e7bef97e73b8830d97c8
Gabriel Araujo <gabrielqa@me.com>
2014-02-07 18:27:44 -0700

    #ZUP-193 Adicionado estrutura básica

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,7 +7,8 @@ angular.module('zupWebAngularApp', [
   'ngSanitize',
   'ngRoute',
   'ui.bootstrap',
-  'ui.mask'
+  'ui.mask',
+  'angularFileUpload'
 ])
 
 .config(function ($routeProvider, $httpProvider) {
@@ -271,7 +272,7 @@ angular.module('zupWebAngularApp', [
     $modal.open({
       templateUrl: 'views/modal_new_report.html',
       windowClass: 'modal_new_report',
-      controller: ['$scope', '$modalInstance', 'Reports', 'Alert', '$route', function($scope, $modalInstance, Reports, Alert, $route) {
+      controller: ['$scope', '$modalInstance', 'Reports', 'Alert', '$route', '$upload', function($scope, $modalInstance, Reports, Alert, $route, $upload) {
 
         $scope.inputs = {
           description: null
@@ -289,6 +290,33 @@ angular.module('zupWebAngularApp', [
 
         $scope.selectCategory = function(categoryData) {
           $scope.categoryData = categoryData;
+        };
+
+        $scope.onFileSelect = function($files) {
+          //$files: an array of files selected, each file has name, size, and type.
+          for (var i = 0; i < $files.length; i++) {
+            var file = $files[i];
+            $scope.upload = $upload.upload({
+              url: 'server/upload/url', //upload.php script, node.js route, or servlet url
+              // method: POST or PUT,
+              // headers: {'headerKey': 'headerValue'},
+              // withCredential: true,
+              data: {myObj: $scope.myModelObj},
+              file: file,
+              // file: $files, //upload multiple files, this feature only works in HTML5 FromData browsers
+              /* set file formData name for 'Content-Desposition' header. Default: 'file' */
+              //fileFormDataName: myFile, //OR for HTML5 multiple upload only a list: ['name1', 'name2', ...]
+              /* customize how data is added to formData. See #40#issuecomment-28612000 for example */
+              //formDataAppender: function(formData, key, val){}
+            }).progress(function(evt) {
+              console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+            }).success(function(data, status, headers, config) {
+              // file is uploaded successfully
+              console.log(data);
+            });
+            //.error(...)
+            //.then(success, error, progress);
+          }
         };
 
         $scope.send = function() {
