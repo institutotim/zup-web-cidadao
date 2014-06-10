91ccd35aff006d88e691e937c40a262c1345d2fc
Anderson Stühler <caos19@gmail.com>
2014-06-10 10:13:41 -0300

    #ZUP-826 #Done

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -2,23 +2,24 @@
 
 angular.module('zupWebAngularApp')
 
-.controller('AccountCtrl', ['$scope', 'Alert', 'Users', 'Reports', function ($scope, Alert, Users, Reports) {
+  .controller('AccountCtrl', ['$scope', 'Alert', 'Users', 'Reports', function ($scope, Alert, Users, Reports) {
 
-  $scope.inputs = {};
+    $scope.inputs = {};
 
     Reports.getMyItems(function(data) {
-        $scope.reportsn = data.reports.length;
+      $scope.reportsn = data.reports.length;
     });
     $scope.submit = function() {
-    $scope.inputErrors = {};
-    $scope.processingForm = true;
-    Users.update($scope.me, function() {
-      $scope.processingForm = false;
-      Alert.show('Parabéns!', 'Dados atualizados com sucesso.');
-    }, function(response) {
-      $scope.processingForm = false;
-      $scope.inputErrors = response.data.error;
-    });
-  };
+      $scope.inputErrors = {};
 
-}]);
+      $scope.processingForm = true;
+      Users.update($scope.me, function() {
+        $scope.processingForm = false;
+        Alert.show('Parabéns!', 'Dados atualizados com sucesso.');
+      }, function(response) {
+        $scope.processingForm = false;
+        $scope.inputErrors = response.data.error;
+      });
+    };
+
+  }]);
