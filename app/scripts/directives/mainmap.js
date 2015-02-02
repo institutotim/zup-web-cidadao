7625f7630e0970a8687779381b9781eea0e71c9c
Gabriel Araujo <gabrielqa@me.com>
2015-02-02 01:53:02 -0800

    lowered limit of items

--- app/scripts/directives/mainmap.js
+++ app/scripts/directives/mainmap.js
@@ -126,7 +126,7 @@ angular.module('zupWebAngularApp')
               'position[latitude]': options.center.lat(),
               'position[longitude]': options.center.lng(),
               'position[distance]': options.distance,
-              'limit': 800,
+              'limit': 100,
               'zoom': mapProvider.map.getZoom()
             };
 
