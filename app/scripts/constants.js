11079d65fd05a160981a84220c442655d8d7e071
Pedro Felipe <pedro@pedrofelipe.com.br>
2015-05-16 23:37:50 -0300

    height automatico na alteracao do cadastro; sem barra de scroll em minhas solicitacoes

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://zup-staging.cognita.ntxdev.com.br',mapLat:'-23.689701',mapLng:'-46.564874',mapZoom:'11'})
+.constant('ENV', {name:'development',apiEndpoint:'http://zup-staging.cognita.ntxdev.com.br',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'13'})
 
 ;
