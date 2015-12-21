942402ed6b97361bd639e11512790691dfd590c9
Nathan Ribeiro <lynxnathan@gmail.com>
2015-12-21 18:28:16 -0200

    Remove karma and ignore logo.png from cache busting

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,10 +41,6 @@ module.exports = function (grunt) {
           livereload: true
         }
       },
-      jsTest: {
-        files: ['test/spec/{,*/}*.js'],
-        tasks: ['newer:jshint:test', 'karma']
-      },
       compass: {
         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         tasks: ['compass:server', 'autoprefixer']
@@ -194,6 +190,7 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '!<%= yeoman.dist %>/images/logo.png',
             '<%= yeoman.dist %>/styles/fonts/*',
             '!<%= yeoman.dist %>/scripts/constants.js',
           ]
