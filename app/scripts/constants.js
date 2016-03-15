25bd6a492103c7e76478bcb4dc0272d910b54a82
David Guilherme <davidlguilherme@gmail.com>
2016-03-15 18:30:54 -0300

    Ignore constants file

--- app/scripts/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('ENV', {name:'development',apiEndpoint:'http://zup-staging.cognita.ntxdev.com.br',mapLat:'-23.549671',mapLng:'-46.6321713',mapZoom:'13'})
-
-;
